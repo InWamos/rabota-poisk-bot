99d607da194ffb50475cdf7c9517fc8e514a6032
InWamos <mykyta.pidgornyi@gmail.com>
2024-11-16 03:28:53 +0200

    Buttons

--- src/buttons/start_buttons.py
+++ src/buttons/start_buttons.py
@@ -4,10 +4,8 @@ from aiogram.types import InlineKeyboardButton
 
 start_button_markup = InlineKeyboardMarkup(
     inline_keyboard=[
-        [
-            InlineKeyboardButton(text="📊Подбор вакансий",callback_data="chats_list"),
-            InlineKeyboardMarkup(text="📈Инструмент",url="https://t.me/Jobs_bridge"),
-            InlineKeyboardMarkup(text="Разместить свою вакансию",callback_data="place_ad"),
-        ]
+        [InlineKeyboardButton(text="📊Подбор вакансий", callback_data="chats_list")],
+        [InlineKeyboardButton(text="📈Инструмент", url="https://t.me/Jobs_bridge")],
+        [InlineKeyboardButton(text="Разместить свою вакансию", callback_data="place_ad")],
     ]
 )
