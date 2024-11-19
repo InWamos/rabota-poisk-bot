95a575bc9362cf0582506fb179aee02902cabff6
InWamos <mykyta.pidgornyi@gmail.com>
2024-11-20 01:21:22 +0200

    initial couple for json

--- src/database_methods/json_database_methods_impl.py
+++ src/database_methods/json_database_methods_impl.py
@@ -8,16 +8,31 @@ from src.database_methods.database_methods import DatabaseMethods
 class JsonDatabaseMethodsImpl(DatabaseMethods):
     PATH_TO_DB: str = "data/db/users.json"
 
-    def add_user(self, user_id: int, date_added: datetime.datetime) -> None:
-        pass
+    def add_user(self, user_id: int) -> None:
+        table: dict[int, dict[str, Any]] = self._get_json_data()
+
+        if not user_id in table.keys():
+            user: dict[str, Any] = {
+                "date": datetime.datetime.now(),
+                "isBanned": False,
+            }
+            table[user_id] = user
+
+            self._write_json_data(table=table)
 
     def get_users_amount(self) -> int:
-        return 0
+        table = self._get_json_data()
+        return len(table.keys())
+
+    def set_user_banned(self, user_id: int) -> None:
+        table = self._get_json_data()
+        table[user_id]["isBanned"] = True
+        self._write_json_data(table=table)
 
     def _get_json_data(self) -> dict[int, dict[str, Any]]:
         with open(file=self.PATH_TO_DB, mode="r") as json_file:
             return json.load(json_file)
 
-    def _is_user_exists(self, user_id: int) -> bool:
-        table = self._get_json_data()
-        return user_id in table.keys()
+    def _write_json_data(self, table: dict[int, dict[str, Any]]) -> None:
+        with open(self.PATH_TO_DB, "w") as json_file:
+            json.dump(table, json_file)
