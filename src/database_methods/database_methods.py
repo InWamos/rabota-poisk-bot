bb2ca7d3752237d660d84dfddb57b9998f213092
InWamos <mykyta.pidgornyi@gmail.com>
2024-11-20 00:59:32 +0200

    Remove unused argument

--- src/database_methods/database_methods.py
+++ src/database_methods/database_methods.py
@@ -1,12 +1,15 @@
-import datetime
 from abc import ABC, abstractmethod
 
 
 class DatabaseMethods(ABC):
     @abstractmethod
-    def add_user(self, user_id: int, date_added: datetime.datetime) -> None:
+    def add_user(self, user_id: int) -> None:
         pass
 
     @abstractmethod
     def get_users_amount(self) -> int:
+        pass
+
+    @abstractmethod
+    def set_user_banned(self) -> None:
         pass
