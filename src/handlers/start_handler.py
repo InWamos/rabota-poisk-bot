6e3bab7dd42c9308665b64651886ea0073932a97
InWamos <mykyta.pidgornyi@gmail.com>
2024-11-20 02:54:16 +0200

    Add logger to db

--- src/handlers/start_handler.py
+++ src/handlers/start_handler.py
@@ -5,6 +5,8 @@ from aiogram.types import Message
 from aiogram.filters import CommandStart
 from buttons.start_buttons import start_button_markup
 
+from database_methods.json_database_methods_impl import JsonDatabaseMethodsImpl
+
 START_PHOTO = os.getenv("START_PHOTO")
 
 router = Router()
@@ -17,3 +19,5 @@ async def respond_to_start_commant(message: Message) -> None:
         caption="💼 Наш проект поможет найти вакансию по твоему запросу а также предоставит инструмент для карьерного роста и заработной платы📈",
         reply_markup=start_button_markup,
     )
+    json_db = JsonDatabaseMethodsImpl()
+    json_db.add_user(str(message.from_user.id))
