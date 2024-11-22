f02111a21cf89c61ebede396931ebffa7aa7ac89
InWamos <mykyta.pidgornyi@gmail.com>
2024-11-22 22:05:14 +0200

    Fix null exception

--- src/handlers/callback_handlers.py
+++ src/handlers/callback_handlers.py
@@ -9,17 +9,22 @@ callback_router = Router()
 
 @callback_router.callback_query(F.data == "chats_list")
 async def return_chats_list(callback: types.CallbackQuery) -> None:
-    await callback.message.edit_reply_markup(reply_markup=chats_button_markup)
+    message = callback.message
+    if isinstance(message, types.Message):
+        await message.edit_reply_markup(reply_markup=chats_button_markup)
 
 
 @callback_router.callback_query(F.data == "back_to_main")
-async def return_chats_list(callback: types.CallbackQuery) -> None:
-    await callback.message.edit_reply_markup(reply_markup=start_button_markup)
+async def back_to_main(callback: types.CallbackQuery) -> None:
+    message = callback.message
+    if isinstance(message, types.Message):
+        await message.edit_reply_markup(reply_markup=start_button_markup)
 
 
 @callback_router.callback_query(F.data == "place_ad")
-async def return_chats_list(callback: types.CallbackQuery) -> None:
-
-    await callback.message.answer(
-        text=f"Для размещения вакансии свяжитесь с менеджером {MANAGER_NICKNAME} с пометкой #РЕКЛАМА"
-    )
+async def place_ad(callback: types.CallbackQuery) -> None:
+    message = callback.message
+    if isinstance(message, types.Message):
+        await message.answer(
+            text=f"Для размещения вакансии свяжитесь с менеджером {MANAGER_NICKNAME} с пометкой #РЕКЛАМА"
+        )
